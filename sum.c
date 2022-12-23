2dc0c6fbf393a66046aa8623bfa456cdc07544c8
chirag3084 <110027137+chirag3084@users.noreply.github.com>
2022-12-23 09:31:33 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+	int i,j,sum;
+	
+	printf("Enter the first number : ");
+	scanf("%d",&i);
+	
+	printf("Enter the second number : ");
+	scanf("%d",&j);
+	
+	sum = i + j;
+	
+	printf("The sum of those numbers is : %d",sum);
+	
+	return 0;
+}
+	
+	
